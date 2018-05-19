6ab7bcd71d2e520c1b86e55a4c817964c9bfe726
Nicolas Marini <nmarini44@gmail.com>
2018-05-19 00:52:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,9 @@ end
 
 def assign_rooms(antendees)
 room_assignments = []
-antendees.each_with_index {|name, room| 
+antendees.each_with_index {|name, room|
 room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"}
- room_assignments 
+ room_assignments
 end
 
 def printer
