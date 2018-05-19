bed9771174d72336602e80d147babdecb39a1d62
Nicolas Marini <nmarini44@gmail.com>
2018-05-19 00:52:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,12 +4,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(antendees)
-antendees.collect{|antendees| badge_maker(antendees)}
+antendees.collect{|names| badge_maker(names)}
 end
 
 def assign_rooms(antendees)
 room_assignments = []
-antendees.each_with_index {|name, room|
+antendees.each_with_index {|name, room| 
 room_assignments << "Hello, #{name}! You'll be assigned to room #{room + 1}!"}
- room_assignments
+ room_assignments 
 end
+
+def printer
